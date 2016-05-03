68ed928fb0a7d0f9f0b4ee86deb88968412ffb07
Miroslav <Miroslav@DESKTOP-TGRJA0G>
2016-05-03 21:53:34 +0300

    Intro Java

---
+++ Java_Developing/src/_1_Language/IntroClass.java
@@ -0,0 +1,41 @@
+package _1_Language;
+
+public class IntroClass {
+
+	/***
+	 * Writing 
+	 * Compiling it into bytecode
+	 * running the bytecode
+	 * 
+	 */
+	
+	
+	
+	/***
+	 * javac - java compiler
+	 * jar - Java Archive
+	 * javadoc - HTML documentation based on Java Source code
+	 *  The java Tool - Execute the java class; load the byte code of the specified class and invokes the main() method java Tool can also run jar files
+	 *  The javadoc Tool - generates HTML documentation from Java source files
+	 *  The jar Tool - create a JAR archive file by combining multiple files of Java application, you can also use the jar tool to extract the content of JAR file
+	 *  The javap Tool - displays information about the methods, variables and parameters present in the classl; javap -help
+	 *  The jarsinger Tool - sign Java ARchive JAR files and verify the signatures of signed JAR files
+	 *  native2ascii Tool - convert files with native encoded characters to a file with Unicode
+	 */  
+	
+	/***
+	 * Compili java with Command line or terminal
+	 * 
+	 * 1. Write your java code(filename.java must be such as java class)
+	 * 2. navigate to the file directory and run command: javac filename.java
+	 * 3. Command will create filename.class file now you can run the java program by typing java filename.class
+	 */
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Java language");
+
+	}
+
+}
